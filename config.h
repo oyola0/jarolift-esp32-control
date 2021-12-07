cfcb090b5f98b5fcfa6da81fbbee0042c20902bb
Jose Oyola <info@josoy.com>
2021-12-07 18:35:59 +0100

    added port of config

--- config.h
+++ config.h
@@ -1,6 +1,11 @@
 
-#define WiFiSSID            "SSID"
-#define WiFiPassword        "WiFiPassword"
+#define WiFiSSID            "WiFi SSID"
+#define WiFiPassword        "WiFi Password"
 
-#define serverName          "http://{id}:{port}"
+#define gpioChangeChannel   32
 
+#define gpioUp              33 
+#define gpioStop            25
+#define gpioDown            26
+
+#define availableChannels   2
